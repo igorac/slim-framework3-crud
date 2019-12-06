ca1df5df4934f825f8d80e8375e5e1d112a105de
Igor AC <igorac1999@gmail.com>
2019-12-06 10:57:59 -0300

    added whoops errors

--- bootstrap.php
+++ bootstrap.php
@@ -4,9 +4,12 @@ session_start();
 
 require "vendor/autoload.php";
 
-
+use App\src\Whoops;
 use Slim\App;
 
 $config['displayErrorDetails'] = true;
 
 $app = new App(['settings' => $config]);
+
+$whoops = new Whoops;
+$whoops->run($app);
